f694e47405dbcc6d4b94b3e5fd47dbc3b2b0eae4
Balaji Rahunathan <mynameisbalag@gmail.com>
2019-10-07 11:06:44 +0530

    Chat blocks added

--- Screens/Discussions.js
+++ Screens/Discussions.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Text, View, AsyncStorage, ActivityIndicator, StyleSheet, ImageBackground, Image, StatusBar } from 'react-native';
+import { Text, View, AsyncStorage, ActivityIndicator, StyleSheet, ImageBackground, Image, StatusBar,KeyboardAvoidingView } from 'react-native';
 import { connect } from 'react-redux';
 import Container from '../Components/Container/Container';
 import FooterComp from '../Components/FooterComp';
 import LinearGradient from 'react-native-linear-gradient';
+import TextInputComp from '../Components/TextInputComp'
 import TitleText from '../Components/TitleText'
 import { Width, background, subTitle } from '../Components/Constants/Constants';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -24,13 +25,74 @@ export default class Discussions extends Component {
                 </View>
                 <View style ={css.contentView}>
 
+
+                <View style={{flex:1,flexDirection:"row",marginHorizontal:10}}>
+                    <View style={{flex:0.15,alignItems:"center"}}>
+                        <View style={{width:40,height:40,borderRadius:20,backgroundColor:"white",justifyContent:"center",alignItems:"center"}}>
+                            <Image style={{width:35,height:35,resizeMode:"contain"}}
+                                    source={require("../assets/icons/ReadMore.png")}/>
+                        </View>
+                    </View>
+                    <View style={{flex:0.9}}>
+                        <View style={{borderRadius:20,backgroundColor:"#D8D8D807"}}>
+                            <View style={{margin:10}}>
+                                <Text style={{fontSize:15,fontWeight:"bold",color:"white"}}>Torsten Paulsson </Text>
+                                <Text style={{fontSize:12,color:"#EBEBF560",paddingBottom:5}}>Manchester <Text style={{fontSize:10,color:"#EBEBF560"}}> 07.00pm saturday</Text></Text>
+                                <Text style={{fontSize:12,color:"white"}}>Certe, inquam, pertinax non possim accommodare torquatos nostros? quos tu tam inportuno tamque crudeli; sin, ut aut in armatum hostem impetum fecisse aut voluptates repudiandae sint et harum quidem rerum necessitatibus saepe eveniet, ut summum bonum esse vult, summumque malum dolorem, idque facere nondum depravatum.</Text>
+                            </View>
+                        </View>
+                    </View>
                 </View>
 
-                <View style={css.footerView}>
-                    <FooterComp name="Comment"
-                        onPress={() => this.props.navigation.navigate("Newsfeed")}>
-                    </FooterComp>
+                <View style={{flex:1,flexDirection:"row",marginHorizontal:10}}>
+                    <View style={{flex:0.9}}>
+                        <View style={{borderRadius:20,backgroundColor:"#D8D8D807"}}>
+                            <View style={{margin:10}}>
+                                <Text style={{fontSize:15,fontWeight:"bold",color:"white"}}>Torsten Paulsson </Text>
+                                <Text style={{fontSize:12,color:"#EBEBF560",paddingBottom:5}}>Manchester <Text style={{fontSize:10,color:"#EBEBF560"}}> 07.00pm saturday</Text></Text>
+                                <Text style={{fontSize:12,color:"white"}}>Certe, inquam, pertinax non possim accommodare torquatos nostros? quos tu tam inportuno tamque crudeli; sin, ut aut in armatum hostem impetum fecisse aut voluptates repudiandae sint et harum quidem rerum necessitatibus saepe eveniet, ut summum bonum esse vult, summumque malum dolorem, idque facere nondum depravatum.</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={{flex:0.15,alignItems:"center"}}>
+                        <View style={{width:40,height:40,borderRadius:20,backgroundColor:"white",justifyContent:"center",alignItems:"center"}}>
+                            <Image style={{width:35,height:35,resizeMode:"contain"}}
+                                    source={require("../assets/icons/ReadMore.png")}/>
+                        </View>
+                    </View>
                 </View>
+
+
+                <View style={{flex:1,flexDirection:"row",marginHorizontal:10}}>
+                    <View style={{flex:0.15,alignItems:"center"}}>
+                        <View style={{width:40,height:40,borderRadius:20,backgroundColor:"white",justifyContent:"center",alignItems:"center"}}>
+                            <Image style={{width:35,height:35,resizeMode:"contain"}}
+                                    source={require("../assets/icons/ReadMore.png")}/>
+                        </View>
+                    </View>
+                    <View style={{flex:0.9}}>
+                        <View style={{borderRadius:20,backgroundColor:"#D8D8D807"}}>
+                            <View style={{margin:10}}>
+                                <Text style={{fontSize:15,fontWeight:"bold",color:"white"}}>Torsten Paulsson </Text>
+                                <Text style={{fontSize:12,color:"#EBEBF560",paddingBottom:5}}>Manchester <Text style={{fontSize:10,color:"#EBEBF560"}}> 07.00pm saturday</Text></Text>
+                                <Text style={{fontSize:12,color:"white"}}>Certe, inquam, pertinax non possim accommodare torquatos nostros? quos tu tam inportuno tamque crudeli; sin, ut aut in armatum hostem impetum fecisse aut voluptates repudiandae sint et harum quidem rerum necessitatibus saepe eveniet, ut summum bonum esse vult, summumque malum dolorem, idque facere nondum depravatum.</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                </View>
+                <KeyboardAvoidingView behavior="position" style={css.footerView} >
+                    <View styles={{width:"100%",flex:0.85,backgroundColor:"white"}}>
+                        <TextInputComp ></TextInputComp>
+                    </View>
+                    <View style={{flex:0.15,alignItems:"center"}}>
+                        <View style={{width:40,height:40,borderRadius:20,backgroundColor:"white",justifyContent:"center",alignItems:"center"}}>
+                            <Image style={{width:40,height:40,resizeMode:"contain"}}
+                                    source={require("../assets/icons/ReadMore.png")}/>
+                        </View>
+                    </View>
+                </KeyboardAvoidingView>
+               
             </Container>
         )
     }
@@ -65,7 +127,9 @@ const css = StyleSheet.create({
     },
     footerView: {
         flex: 0.1,
-        justifyContent: 'flex-end',
+        alignItems:"center",
+        flexDirection:"row",
+        // backgroundColor:"white"
     },
     titleView: {
         flex:0.1,
